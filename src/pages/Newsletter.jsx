546784b530a8b8e0ad1e93880206d41c122a2e50
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-07 13:14:27 -0700

    Newsletter: disable submit button while submitting

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,6 +1,6 @@
 import { Form } from 'react-router-dom';
 import axios from 'axios'
-import { redirect } from 'react-router-dom';
+import { redirect, useNavigation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 
@@ -19,6 +19,8 @@ export const action = async ({ request }) => {
   }
 };
 const Newsletter = () => {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
     return (
       <Form className='form' method='POST'>
         <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -65,11 +67,12 @@ const Newsletter = () => {
           />
         </div>
         <button
-          type='submit'
-          className='btn btn-block'
-          style={{ marginTop: '0.5rem' }}
+            type='submit'
+            className='btn btn-block'
+            style={{ marginTop: '0.5rem' }}
+            disabled={isSubmitting}
         >
-          submit
+            {isSubmitting ? 'submitting...' : 'submit'}
         </button>
       </Form>
     );
