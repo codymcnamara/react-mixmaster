23a63d7d134b0ba0240c8b576c2eabafc73f7324
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-07 12:24:57 -0700

    add comment

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,8 +1,9 @@
 import { Link, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/CocktailCard';
 const CocktailCard = ({ image, name, id, info, glass }) => {
-  // const data = useOutletContext();
-  // console.log(data);
+    // global context set up in Home Layout(not needed for this project):   
+    // const data = useOutletContext();
+    // console.log(data);
   return (
     <Wrapper>
       <div className='img-container'>
