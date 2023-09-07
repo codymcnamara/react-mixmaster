f293131d961ab745f1e89dcd54b09ac18fc9314b
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-07 13:26:12 -0700

    impliment SearchForm functionality

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,10 +5,11 @@ import SearchForm from '../components/SearchForm';
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () => {
-  const searchTerm = 'margarita';
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: response.data.drinks, searchTerm };
+  export const loader = async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get('search') || '';
+    const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+    return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
@@ -16,7 +17,7 @@ const Landing = () => {
   
     return (
       <>
-        <SearchForm />
+        <SearchForm searchTerm={searchTerm}/>
         <CocktailList drinks={drinks} />
       </>
     );
