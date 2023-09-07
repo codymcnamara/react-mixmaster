fa02b14e3d44b1d41774861b8cf4da227cbcaed5
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-07 12:23:04 -0700

    setup global loading and context

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,21 @@
-import { Outlet } from "react-router-dom";
-import Navbar from '../components/Navbar'
-
-const HomeLayout = ()=> {
-    return(
-        <>
-            <Navbar/>
-            <section className="page">
-                <Outlet/>
-            </section>
-        </>
-    )
-}
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import { useNavigation } from 'react-router-dom';
+const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+  const value = 'some value';
+  return (
+    <>
+      <Navbar />
+      <section className='page'>
+        {isPageLoading ? (
+          <div className='loading' />
+        ) : (
+          <Outlet context={{ value }} />
+        )}
+      </section>
+    </>
+  );
+};
 export default HomeLayout;
