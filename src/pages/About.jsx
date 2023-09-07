85f6a53cc0ba5e1483b578aec55a547d6be3dc57
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-07 01:40:40 -0700

    set up Links on home and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
     return (
       <div>
         <h1>About</h1>
+        <Link to ="/">Home Page</Link>
       </div>
     );
   };
